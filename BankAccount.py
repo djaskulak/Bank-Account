ff4d83cd14cde0951a14dc8ab2f2b434178a1754
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-02-02 16:01:49 -0800

    created comments
    outlined the file for what needs to be included and started building the class

---
+++ BankAccount.py
@@ -0,0 +1,19 @@
+""" CS 1.1 Bank Account """
+""" by Danika Jaskulak-Gonzalez """
+
+# define bank account class
+  """ methods needed: 
+      * deposit(balance and amount)
+      * withdraw(balance and amount)
+      * get_balance(print and return)
+      * add_interest(interest = balance *  0.00083)
+      * print_receipt(name, account number, routing number, balance)
+  """
+class BankAccount:
+
+  def __init__(self, name, account_num, routing_num, balance):
+    
+
+# make 3 different bank accounts
+
+""" STRETCH CHALLENGE: make terminal ATM, with charge for use """
